0e97b0424a619e662e5291fe1a6889c455f36c92
HendrikRassmann <hendrik.rassmann@tu-dortmund.de>
2020-11-05 12:40:41 +0100

    i hate my life

--- Simulation/MainNoDB.py
+++ Simulation/MainNoDB.py
@@ -22,28 +22,69 @@ import math
 #importers
 import importlib.util
 
+#fakeDB
+import json
+
+
+xValueConverter = {
+        "numberOfJobs":0,
+        "numberOfNodes":1,
+        "seqR":2,
+        "largeR":3,
+        "timespan":4,
+        "minSeq":5,
+        "maxSeq":6,
+        "minPar":7,
+        "maxPar":8
+}
 #from collections import defaultdict
 
 def main(experiment):
+        print ("Started Runs")
         start: float = timeit.default_timer()
+
         finishedRuns = []
-        numberOfIterations = list(range(1))
-        product = itertools.product( *(list(experiment.values()))[:-1] )
+        numberOfIterations = list(range(2))    
+
+        byTarget = {
+                "makespan":{},
+                "avgFlowTime":{},
+                "maximumLateness":{}
+        }
+        for target in byTarget:
+                for sf in experiment["schedulers"]:
+                        byTarget[target][sf]=[]
+        
+        product = itertools.product( *(list(experiment.values()))[:-2] )
         for conf in product:#itertools.product(numberOfJobs,numberOfNodes,seqR,largeR,timespan,minSeq,maxSeq,minPar,maxPar):
 
+                      
                 for i in numberOfIterations:
-                        jobs: List[Simulation.Job] = Generator.generate(*conf[:-1])
-                        sys: Simulation.System = Simulation.System(jobs.copy(),conf[1],conf[-1])
-                        finished: List[Simulation.Job] = sys.run()
-                        finishedRuns.append(finished)
-                        print (Analysis.standardAnalysis(finished))
-        stop = timeit.default_timer()
-        print('Time: ', stop - start)
-        
-def show():
-	pass
-	#talk to db
-	#plot what you want
+                        jobs: List[Simulation.Job] = Generator.generate(*conf)
+                        for sf in experiment["schedulers"]:
+                                sys: Simulation.System = Simulation.System(jobs.copy(),conf[1],sf)
+                                finished: List[Simulation.Job] = sys.run()
+                                analysis = Analysis.standardAnalysis(finished)
+                                for target in byTarget:
+                                        byTarget[target][sf].append((conf[xValueConverter[experiment["x-axis"][0]]],analysis[target]))
+
+
+        print (byTarget)
+        '''dbCopy = {**bySchedulers}
+        for targetFunktion in ["makespan","avgFlowTime","maxiumumLateness"]:
+                #setup
+                xLabel = experiment["x-axis"][0]
+                yLabel = targetFunktion
+                for sf in experiment["schedulers"]:
+                        confs = dbCopy[sf]#confs : {(*conf):[{Analysis1},{Analysis} ]}
+                        #avg out
+                        #fetch and plot
+                        xs = []
+                        ys = []
+                        for conf in confs:
+                                xs.append()
+                                acc = sum(map(lambda x : x[targetFunktion], confs[conf]))
+                        confs'''
 
 if __name__ == "__main__":
         if len (sys.argv) == 0:
@@ -52,9 +93,6 @@ if __name__ == "__main__":
         if sys.argv[1].endswith(".py"):
                 file_path = sys.argv[1]
                 module_name = sys.argv[1][:-3]
-                #import experiment1
-                #print ("testy")
-                #print ("imported file :D")
                 spec = importlib.util.spec_from_file_location(module_name, file_path)
                 module = importlib.util.module_from_spec(spec)
                 sys.modules[module_name] = module
@@ -62,9 +100,6 @@ if __name__ == "__main__":
                 main(module.experiment)
                 #print (module.experiment)
                 sys.exit()
-
-        if sys.argv[1] == "show":
-                Analysis.show()
 
         else:
                 print ("run with argument halp for help")
